596648c2a531d94780139d969e317ae33ccb0781
Marikka Højen <mari45re@stud.kea.dk>
2022-04-22 11:47:07 +0200

    page-projekter console.log start

--- page-projekter.php
+++ page-projekter.php
@@ -14,6 +14,10 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+<h1 id="overskrift">Projekter</h1>
+<section id="projekt-oversigt"></section>
+		</main><!-- #main -->
+
 <template>
 	<article>
 		<img src="" alt="">
@@ -23,10 +27,27 @@ get_header();
 
 <script>
 	console.log("så er vi i gang")
+
+	let projekter = [];
+	let categories;
+	let filterProjekt = "alle";
+
+	const liste = document.querySelector("#projekt-oversigt")
+	const skabelon = document.querySelector("template")
+	document.addEventListener("DOMContentLoaded", start);
+
+	const url = "https://mariksen.dk/kea/2-semester/09_cms/unesco_site/wp-json/wp/v2/projekt";
+	const catUrl = "https://mariksen.dk/kea/2-semester/09_cms/unesco_site/wp-json/wp/v2/categories";
+
+function start(){
+	console.log("nu er vi i start")
+	hentData();
+}
+
 </script>
 			
 
-		</main><!-- #main -->
+		
 	</div><!-- #primary -->
 
 <?php
