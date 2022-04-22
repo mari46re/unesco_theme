1c85b43ab5faea1ce7febe1096c69473fd3a2601
Frederik Heller <93513730+fred04q4@users.noreply.github.com>
2022-04-22 14:14:31 +0200

    text og h1

--- page-skoler.php
+++ page-skoler.php
@@ -14,6 +14,8 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+		<h1 id="overskrift">Skoler i Netværket</h1>
+		<p>Se listen over de mange skoler, der er en del af det danske UNESCO ASP-netværk. Find også skolernes hjemmesider samt oplysningerne på de individuelle skolers kontaktpersoner.</p>
 		<section id="skole-oversigt"> </section>
 
 			
