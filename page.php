23ee0d55116c213c31621b99b052ce5ae4f189a3
Marikka Højen <mari45re@stud.kea.dk>
2022-04-29 12:54:06 +0200

    header

--- page.php
+++ page.php
@@ -38,11 +38,11 @@ get_header();
 
 function scrollFunction() {
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		document.getElementById("masthead").style.backgroundColor = "#f7f7f3e6";
+		document.getElementById("masthead").style.backgroundColor = "#f7f7f3";
 		document.getElementById("logo").style.padding = "1rem"
 		
 	} else {
-		document.getElementById("masthead").style.backgroundColor = "none";
+		document.getElementById("masthead").style.backgroundColor = "#f7f7f3e6";
 }
 }
 		</script>
